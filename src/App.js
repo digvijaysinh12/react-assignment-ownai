8a821588ebdca0060feabeb6d71fb8fefeb3a324
digvijaysinh12 <sarvaiyaraa040@gmail.com>
2025-10-16 14:53:47 +0530

    Component Render in this page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from "react";
+import "./App.css";
+import PurchaseOrderForm from "./componets/PurchaseOrderForm";
+import { POProvider } from "./context/POContext";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <POProvider>
+      <div className="App">
+        <PurchaseOrderForm />
+      </div>
+    </POProvider>
   );
 }
 
